bcbda1de51c297fe5c7428ab811194b7603a3518
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2022-01-31 22:27:08 +0900

    #1 changed to smb

--- Docker/db/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-chown mysql:mysql -R /var/lib/mysql
-
-/usr/local/bin/docker-entrypoint.sh mariadbd --skip-innodb-read-only-compressed
-
--- Docker/nextcloud/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-chown www-data:root -R /var/www/html
-chown www-data:www-data .htaccess
-
-/entrypoint.sh apache2-foreground
-
